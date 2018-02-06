d9d41d69687bb2fdf69a5f981dc0548a89e965db
eqmvii <coelomate@gmail.com>
2018-02-06 12:48:39 -0500

    more fizzbuzz updates

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -3,10 +3,15 @@
 
 defmodule FizzBuzz do
     
+    # sub function call to mask the awkward arity (i.e. must have three arguments
+    # to make the recursion work, but somebody using the function would expect to
+    # only need to send one argument, the number of fizzbuzz numbrers to print
     def upto(n) when n > 0, do: _upto(1, n, [])
 
+    # base recursive case - return the list in reverse order
     def _upto(_current, 0, result), do: Enum.reverse result
 
+    # recursive case 
     def _upto(current, remaining, result) do
         next_answer = 
             cond do
@@ -16,10 +21,15 @@ defmodule FizzBuzz do
                     "Fizz"
                 rem(current, 5) == 0 ->
                     "Buzz"
-                # a default case
+                # a default case for the cond statement
+                # that will execute if none of the prior 
+                # statements evaluate to true
                 true ->
                     current
             end
+            # recursive call to _upto, increasing current by 1, reducing remaining by 1,
+            # and sending a new list with next_answer as the head of the list and 
+            # result, all of the old numbers, as the tail of the list
             _upto(current+1, remaining-1, [next_answer | result ])
     end
 end
